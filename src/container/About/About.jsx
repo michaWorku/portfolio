bbd820c12700833869ae89fe0d94263c609e8404
michaworku <mikias27worku@gmail.com>
2022-03-29 18:26:09 +0300

    about UI design

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,8 +1,38 @@
 import './About.scss'
+import { motion } from  'framer-motion'
+
+import { images } from '../../constants'
+
+const abouts = [
+  {title : 'Web Development', description: 'I am a good web developer', imgUrl: images.about01},
+  {title : 'Frontend Development', description: 'I am a good Frontend web developer', imgUrl: images.about02},
+  {title : 'Backend Developmenjt', description: 'I am a good Backend web developer', imgUrl: images.about03},
+  {title : 'MERN Stack', description: 'I am a good MERN stack developer', imgUrl: images.about04}
+]
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
+
+      <div className="app__profiles">
+        {
+          abouts.map((about, index)=>(
+            <motion.div
+              whileInView={{opacity:1}}
+              whileHover={{scale: 1.1}}
+              transitio={{duration: 0.5, type: 'tween'}}
+              className='app__profiles-item'
+              key={about.title + index}
+            >
+              <img src={about.imgUrl} alt={about.title} />
+              <h2 className="bold-text" style={{marginTop: 20}}>{about.title}</h2>
+              <p className="p-text" style={{marginTop: 20}}>{about.description}</p>
+            </motion.div>
+          ))
+        }
+      </div>
+    </>
   )
 }
 
