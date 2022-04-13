c9f1b8553919a47b692948bfbf83c2ae5084666a
michaworku <mikias27worku@gmail.com>
2022-04-13 09:20:29 +0300

    adding media quirey and social media links

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { BsTwitter, BsLinkedin } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
-    <div> 
-      <BsTwitter />
+    <div>
+      <a href="https://linkedin.com/in/mikias-worku-8138b8183" target="_blank" rel="noreferrer"> 
+        <BsLinkedin />
+      </a>
     </div>
     <div>
-      <FaFacebookF />
+      <a href="https://twitter.com/mikias_worku" target="_blank" rel="noreferrer"> 
+        <BsTwitter />
+      </a>
     </div>
     <div>
-      <BsInstagram />
+      <a href="https://www.facebook.com/mikias.workuW" target="_blank" rel="noreferrer"> 
+        <FaFacebookF />
+      </a>   
     </div>
+    
   </div>
 );
 
